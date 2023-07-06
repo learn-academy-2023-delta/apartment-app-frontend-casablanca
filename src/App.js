f43de579b643642d473159d0c2719628a6ca6f0b
Ricky Espinoza <rickyjespinoza@gmail.com>
2023-07-06 11:28:45 -0700

    not done yet. passing it off to jacob

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import ApartmentEdit from './pages/ApartmentEdit'
+import ApartmentIndex from './pages/ApartmentIndex'
+import ApartmentNew from './pages/ApartmentNew'
+import ApartmentProtectedIndex from './pages/ApartmentProtectedIndex'
+import ApartmentShow from './pages/ApartmentShow'
+import NotFound from './pages/NotFound'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apartmentedit" element={<ApartmentEdit/>} />
+          <Route path="/apartmentindex" element={<ApartmentIndex/>} />
+          <Route path="/apartmentnew" element={<ApartmentNew/>} />
+          <Route path="/apartmentprotectedindex" element={<ApartmentProtectedIndex/>} />
+          <Route path="/apartmentshow" element={<ApartmentShow/>} />
+          <Route path="/notfound" element={<NotFound/>} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/sigup" element={<SignUp/>} />
+        </Routes>
+    </>
   );
 }
 
